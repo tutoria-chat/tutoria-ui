331c603225aac6a5648e6e5babd3bcd431731555
Mateus <mateuspalacio@gmail.com>
2025-10-02 20:42:29 -0300

    ajain

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { ProtectedRoute } from '@/components/auth/protected-route';
 import { DashboardLayoutWrapper } from '@/components/layout/dashboard-layout-wrapper';
 
+export const dynamic = 'force-dynamic';
+
 export default function DashboardLayout({
   children,
 }: {
